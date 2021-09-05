c69d2ffd270092b20654ea086247184fdf228754
rui2015 <15705566+rui2015@users.noreply.github.com>
2021-09-05 21:26:11 +0100

    Version bump to 1.0.0-rc.1

--- Android.mk
+++ Android.mk
@@ -47,7 +47,7 @@ LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
 LOCAL_SHARED_LIBRARIES += codegen_0_13_0
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"SearchFixes"' -DVERSION='"0.1.0"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
+LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"SearchFixes"' -DVERSION='"1.0.0-rc.1"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
 LOCAL_C_INCLUDES += ./include ./src
 include $(BUILD_SHARED_LIBRARY)
